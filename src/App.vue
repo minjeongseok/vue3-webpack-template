19febefb9cd2e7ea19cbe3e44558c23293298b1d
minjeongseok <mjs1677@gmail.com>
2023-10-20 16:51:56 +0900

    create project

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>{{ message }}</h1>
+    <Hello />
+</template>
+
+<script>
+import Hello from "~/components/Hello";
+
+export default {
+    components: {
+        Hello,
+    },
+    data() {
+        return {
+            message: "App.vue message: Hello Vue3 !",
+        };
+    },
+};
+</script>
---
+++ src/components/Hello.vue
@@ -0,0 +1,3 @@
+<template>
+    Hello.vue: Hello Vue3 !
+</template>
